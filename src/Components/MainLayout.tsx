3bd986509709fcfb017207b0e38181e97a029426
Sophia Zhang <ssszhang05@gmail.com>
2025-04-01 22:47:20 -0400

    Added pages for the basic and detailed quizzes, as well as buttons on the homepage and navbar directing to the pages.

--- src/Components/MainLayout.tsx
+++ src/Components/MainLayout.tsx
@@ -1,63 +1,36 @@
 // Abrar: I moved our original App.tsx into its own component. So our layouts would be along these lines
 
 // src/components/MainLayout.tsx
-import React, { useState } from 'react';
+import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import { Button, Form } from 'react-bootstrap';
 import logo from '../logo.svg';
 
-// Local storage for API Key
-let keyData = "";
-const saveKeyData = "MYKEY";
-const prevKey = localStorage.getItem(saveKeyData);
-if (prevKey !== null) {
-  keyData = JSON.parse(prevKey);
-}
 
 function MainLayout() {
-  const [key, setKey] = useState<string>(keyData);
-  
-  function handleSubmit() {
-    localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload();
-  }
-
-  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
-    setKey(event.target.value);
-  }
-  
   return (
     <div className="App">
       
-        {/* Navigation */}
-        <nav className="main-nav mt-4">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/test">Pages Tutorial</Link>
-            </li>
-          </ul>
-        </nav>
-        
-        {/* This is where the routed content will appear */}
-        <div className="content-area mt-4">
-          <Outlet />
-
-        
-        
-        <div className="api-container" >
-        <Form className="mt-4">
-          <Form.Label>Enter API Key:</Form.Label>
-          <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-          <br></br>
-          <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-        </Form>
-        </div>
+      {/* Navigation */}
+      <nav className="main-nav mt-4">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/basic-quiz">Basic Quiz</Link>
+          </li>
+          <li>
+            <Link to="/detailed-quiz">Detailed Quiz</Link>
+          </li>
+        </ul>
+      </nav>
+      
+      {/* This is where the routed content will appear */}
+      <div className="content-area mt-4">
+        <Outlet />
       </div>
 
       <header className="App-footer">
---
+++ src/Pages/BasicQuiz.tsx
@@ -0,0 +1,13 @@
+// src/Pages/Basic-Quiz.tsx
+import React from 'react';
+
+function Basic_Quiz() {
+  return (
+    <div className="page-content">
+      <h2>Basic Quiz</h2>
+      <p>it's basic quizzing time</p>
+    </div>
+  );
+}
+
+export default Basic_Quiz;
