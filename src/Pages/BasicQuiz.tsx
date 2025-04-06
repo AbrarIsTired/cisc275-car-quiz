535c99789780310881d7fb2a32c45ce89aa4eddb
AbrarIsTired <abraramn@udel.edu>
2025-04-06 18:12:45 -0400

    Added interface to hold all end user inputs

--- src/Pages/BasicQuiz.tsx
+++ src/Pages/BasicQuiz.tsx
@@ -1,6 +1,17 @@
 // src/Pages/Basic-Quiz.tsx
 import React from 'react';
 
+interface FormData{
+  educationLevel: string;
+  workLifeBalance: string;
+  workPreference: string;
+  workEnviroment: string;
+  salaryRange: string;
+  motivations: string[];
+  publicSpeaking: string;
+}
+
+
 function Basic_Quiz() {
   return (
     <div className="page-content">
