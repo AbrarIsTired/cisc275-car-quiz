30b8fd34e321c81e60085e56f4d934a23d7e2368
AbrarIsTired <abraramn@udel.edu>
2025-04-08 18:32:57 -0400

    Resetting to Original

--- src/Pages/BasicQuiz.tsx
+++ src/Pages/BasicQuiz.tsx
@@ -1,17 +1,6 @@
 // src/Pages/Basic-Quiz.tsx
 import React from 'react';
 
-interface FormData{
-  educationLevel: string;
-  workLifeBalance: string;
-  workPreference: string;
-  workEnviroment: string;
-  salaryRange: string;
-  motivations: string[];
-  publicSpeaking: string;
-}
-
-
 function Basic_Quiz() {
   return (
     <div className="page-content">
