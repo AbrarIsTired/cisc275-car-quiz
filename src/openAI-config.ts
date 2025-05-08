46ecc8633ebc9e1e22eb8e2a5c637b341361f20b
AbrarIsTired <abraramn@udel.edu>
2025-05-08 14:07:26 -0400

    Cleaned up old/commented code

--- src/openAI-config.ts
+++ src/openAI-config.ts
@@ -35,23 +35,3 @@ export async function callOpenAI_API(message: string) {
   }
 
 }
-/*
-export async function callOpenAI_API(message: string) {
-    const response = await client.chat.completions.create({
-    model: "gpt-4o",
-    messages: [{
-            role: "user", 
-            content: message
-        }, {
-            role: "system",
-            content: `Find the ideal career for the user based on a summary of their preferences and skills. 
-                Then, provide detailed information for this career, including salary, demand, required skillsets, and related fields.`
-        }],
-        temperature: 0.5,
-    
-    }); 
-    const reply = response.choices[0]?.message?.content
-    console.log(response.choices[0]?.message?.content);
-    return reply;
-}
-*/
